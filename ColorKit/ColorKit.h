4e6f3291e49ef2a487641b6a80e07df30116e10b
Lorenzo Bernaschina <lorenzo.bernaschina@mail.polimi.it>
2019-01-02 00:05:10 +0100

    First commit

---
+++ ColorKit/ColorKit.h
@@ -0,0 +1,19 @@
+//
+//  ColorKit.h
+//  ColorKit
+//
+//  Created by Lorenzo Bernaschina on 02/01/2019.
+//  Copyright © 2019 Lorenzo Bernaschina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorKit.
+FOUNDATION_EXPORT double ColorKitVersionNumber;
+
+//! Project version string for ColorKit.
+FOUNDATION_EXPORT const unsigned char ColorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorKit/PublicHeader.h>
+
+
